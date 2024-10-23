73580527bd1756cc3cfeb2f1d974101b2b589401
prabal2005 <lucky264209@gmail.com>
2024-10-23 15:45:14 +0530

    hbs and partials done

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDb();
 const app = express();
 const port= process.env.PORT || 5000;
-
+app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(cors());
 //error handling middleware
@@ -20,6 +20,17 @@ app.use(errorHandler);
 app.get('/',(req,res)=>{
     res.send("working")
 });
+app.get("/home",(req,res)=>{
+    res.render("home",{})  
+})
+app.get("/allusers",(req,res)=>{
+    res.render("user",{
+        users:[{id:1,username:"prabal",age:19},
+            {id:2,username:"ram",age:20}]
+    })
+})
+
+
 app.listen(port,()=>{
     console.log(`Server is running on port http://localhost:${port}`);
 });
